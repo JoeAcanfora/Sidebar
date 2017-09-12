b43cae52c041fd849380ee6fb7dbf8d855315b81
Joe Acanfora <joeaca04@gmail.com>
2017-09-11 22:30:03 -0400

    added firebase UI

---
+++ app/src/main/java/chat/sidebar/sidebar/model/Message.java
@@ -0,0 +1,15 @@
+package chat.sidebar.sidebar.model;
+
+import java.util.List;
+
+/**
+ * Created by joeacanfora on 9/11/17.
+ */
+
+public class Message {
+
+    private List<String> directMessageUserIds;
+    private String roomId;
+    private String uniqueId;
+    private String senderUserId;
+}
